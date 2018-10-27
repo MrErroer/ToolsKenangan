c7386774cd0aec81af560f36cffc185a5a2c9576
Mr_Error <44495404+MrErroer@users.noreply.github.com>
2018-10-27 07:29:41 +0700

    Add files via upload

---
+++ KENANGAN.py
@@ -0,0 +1,79 @@
+import time
+import os,sys
+
+blue = '\033[34;1m'
+green = '\033[32;1m'
+purple = '\033[35;1m'
+cyan = '\033[36;1m'
+red = '\033[31;1m'
+white = '\033[37;1m'
+yellow = '\033[33;1m'
+
+os.system ('clear')
+
+print(yellow),"   _____________________________________________"
+time.sleep(0.23)
+print(red),"   |____________________________________________|"
+time.sleep(0.24)
+print(purple),"   |           --------Author--------           |"
+time.sleep(0.24)
+print(blue),"   |                  MR.ERROR                  |"
+time.sleep(0.24)
+print(green),"   |              >>> KENANGAN GC <<<           |"
+time.sleep(0.24)
+print(yellow),"   |/______\/______\____________/______\/______\|"
+time.sleep (2.2)
+print("\n")
+print(red),("[1 ] My Friend")
+print(green),("[2 ] Exit")
+print(white),("[3 ] About")
+print(yellow),("[+]------------------------------------------------[-]")
+print(green),"[ Script By:",(blue),"MR.ERROR.                              ]"
+print(green),"[ Since    :",(white),"26 OKTOBER 2018                        ]"
+print(green),"[ Support  :",(yellow),"All who have liked the page            ]"
+print(green),"[ Team Cyber  :",(red),"Anonymous Big Family                ]"
+print(yellow),("[+]------------------------------------------------[-]")
+MrError = input("\033[1;91mPilih \033[1;37m=> ")
+
+if MrError ==2:
+    time.sleep(0.5)
+    print(green),("MR.ERROR")
+    time.sleep (1.4)
+    print (red),("All My Grup Cyber")
+    time.sleep (1.9)
+    print (blue),("Dont Recode!!!:")
+    time.sleep (1.5)
+    print(green),("Ini Bukan Tools")
+    time.sleep (1.9)
+    print(red),("Tapi Kenangan Grup cyber")
+    time.sleep (1.95)
+    print(blue),("Fuck The System")
+    time.sleep (1.9)
+if MrError ==1:
+	os.system ('clear')
+	os.system ('figlet My Friend|lolcat')
+	time.sleep (1.5)
+	os.system ('clear')
+	os.system ('figlet MR.ERROR|lolcat')
+	time.sleep (2.2)
+	os.system ('clear')
+	os.system ('figlet Anonymous Big Family|lolcat')
+	print(red),('-Virgiawan -Mr.Kzy -Mr.Cuplish -Tu4n 4K427 -Rzkape -MR.GOOD -MR.CABE -MR.DUCK -MR.JAVA -MR.KOSONG -MR.PRO -MR.UPIX -MR.DDOS -TUAN SILET -INYOURS3RVER -TU4N B4DUT -WHO AM I -DarkRipper -Mr.Noob -Mr.z.404! -INYOUR./TU4NN1CKN4M3 -Mahzul -Ilham -Fauzy -Hendrik Cyber -raka -Vzb Pro -N0OB T3RS4K1T1 -MR.LUXURY -JHONSON -ADIT ART -MR.MADAGASKAR -MR.KOCOMON-M.A -MR DEAD -FAIZANDRA S -Tn Nobita -Ahmad Wildan -MrXCLOWN')
+	os.system ('figlet L45K4R CYB3R|lolcat')
+	time.sleep (1.5)
+	print(blue),('-Gani -BEE -Its Better To Die -Luvas_420 -unknown -MR.GATEFIRE -Pr3ssF')
+	os.system ('figlet KURCACI CYBER TEAM|lolcat')
+	time.sleep (1.1)
+	print(yellow),('-MR.T3R54K1T1 -TU4N ZY12EX -MR.AR -~|Mr.eFPe|')
+if MrError ==3:
+	time.sleep(1.5)
+	print(red),('Ini Hanya Tools Kenangan Mr.Error Kepada Grup Cyber Yg Tlah Mengajarkan dia Sampai Pro')
+	time.sleep(1.5)
+	print(blue),('maaf jika sederhana karena ini hanya tools kenangan Member Grup Cyber Yg Tlah Mendukung/Mengajarkan Mr.Error')
+	time.sleep(1.5)
+	print(red),("[1 ] Exit")
+	MrError = input("\033[1;91mPilih \033[1;37m=> ")
+	if MrError ==1:
+		print(red),('Jika Anda Tidak Suka Abaikan')
+		print(blue),('jika anda suka share tools ini kepada teman kalian')
+	
